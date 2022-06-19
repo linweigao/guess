b89b00fde4c7e7cdd2109d26380a2d702edea527
Linwei Gao <jogao1983@gmail.com>
2022-06-18 18:52:22 -0700

    web layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_frame/flutter_web_frame.dart';
 import 'package:guess/screen_start.dart';
 
 void main() {
@@ -11,24 +13,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: '脑洞大猜',
-        debugShowCheckedModeBanner: false,
-        darkTheme:
-            ThemeData(brightness: Brightness.dark, primarySwatch: Colors.grey),
-        themeMode: ThemeMode.dark,
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-        ),
-        home: const StartScreen());
+    return FlutterWebFrame(
+        builder: (context) {
+          return MaterialApp(
+              title: '脑洞大猜',
+              debugShowCheckedModeBanner: false,
+              darkTheme: ThemeData(
+                  brightness: Brightness.dark, primarySwatch: Colors.grey),
+              themeMode: ThemeMode.dark,
+              theme: ThemeData(
+                // This is the theme of your application.
+                //
+                // Try running your application with "flutter run". You'll see the
+                // application has a blue toolbar. Then, without quitting the app, try
+                // changing the primarySwatch below to Colors.green and then invoke
+                // "hot reload" (press "r" in the console where you ran "flutter run",
+                // or simply save your changes to "hot reload" in a Flutter IDE).
+                // Notice that the counter didn't reset back to zero; the application
+                // is not restarted.
+                primarySwatch: Colors.blue,
+              ),
+              home: const StartScreen());
+        },
+        maximumSize: const Size(800, double.infinity),
+        enabled: kIsWeb,
+        backgroundColor: Colors.grey);
   }
 }
